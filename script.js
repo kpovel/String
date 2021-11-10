8ecb657186cafadcceeb9db58991f4500faa036d
kpovel <kar.pavel123@gamil.com>
2021-11-11 00:35:15 +0200

    index

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function showVerticalMessage(str) {
     if (!str) {
         console.log('Error!')
-    } else {
-        if (str[0] === 'm') {
-            str = (str[0].toUpperCase() + str.slice(1, 10))
-        } else {
-            str = str.slice(0, 10)
-        }
-        for (str of str) {
-            console.log(str)
-        }
+        return str
     }
-
+    if (str[0] === 'm') {
+        str = (str[0].toUpperCase() + str.slice(1, 10))
+    } else {
+        str = str.slice(0, 10)
+    }
+    for (str of str) {
+        console.log(str)
+    }
 }
 
-showVerticalMessage('marathon')
+showVerticalMessage()
