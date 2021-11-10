3c8bdbace963f82ab37503566e3a429e835c2bed
kpovel <kar.pavel123@gamil.com>
2021-11-11 00:21:38 +0200

    index

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function showVerticalMessage(str) {
     if (!str) {
         console.log('Error!')
-    } else if (str[0] === 'm') {
-        str = (str[0].toUpperCase() + str.slice(1, 10))
     } else {
-        str = str.slice(0, 10)
+        if (str[0] === 'm') {
+            result = (str[0].toUpperCase() + str.slice(1, 10))
+        } else {
+            result = str.slice(0, 10)
+        }
+        for (result of result) {
+            console.log(result)
+        }
     }
-    for (str of str) {
-        console.log(str)
-    }
+
 }
 
 showVerticalMessage('marathon')
