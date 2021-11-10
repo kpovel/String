c919dcd2f51e36f36f6628648c54150940080928
kpovel <kar.pavel123@gamil.com>
2021-11-10 09:01:33 +0200

    index

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showVerticalMessage(str) {
     let result
-    if (!str || str.length > 10) {
+    if (!str ) {
         console.log('Error!')
     } else {
-        result = (str[0].toUpperCase() + str.slice(1))
+        result = (str[0].toUpperCase() + str.slice(1, 10))
         for (result of result) {
             console.log(result)
         }
